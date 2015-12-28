5530e8118a539da47d4c7ac98e5abd418e54d0cd
alundgren <andreas.lundgren@gmail.com>
2015-12-28 06:51:34 +0100

    changing the rules

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 A simple game where each player creates a strategy to play the below number guessing game.
 
 Game:
-- A random number between 1 and 1000 is picked
-- Each player takes turns guessing until a player guesses the correct number. That player wins.
-- A player cannot guess a number outside 1 -> 1000 or a number that has already been guessed by either player.
+- A random number between 1 and 1000 is picked.
+- Each player takes turns guessing a number. The next player receives an answer of: first to guess (0), higher (1) or lower (-1).
+- The first player to get correct wins.
--- engine.js
+++ engine.js
@@ -1,24 +1,16 @@
 nrGsr = (function ()  {
-  var createStrategyString = function(s) {
-    return btoa(JSON.stringify({ name : s.name, f : s.f}));    
+  var createStrategyString = function(f) {
+    return btoa(f);    
   }
   var parseStrategyString = function (s) {
-    var c = JSON.parse(atob(s));
-    var ff = Function('g', c.f); 
-    return {
-      name : c.name,
-      f : ff
-    }
+    var c = atob(s);
+    return Function('g', 'r', 'm', c); 
   }
-  var  simulateGame = function (strategyString1, strategyString2, options) {  
-    var strategy1 = parseStrategyString(strategyString1);
-    var strategy2 = parseStrategyString(strategyString2);
-    if(strategy1.name === strategy2.name) {
-      throw 'duplicate strategy names'
-    }
-    var strats = [strategy1, strategy2];
+  var  simulateGame = function (strategy1, strategy2, options) {  
+    var strats = [{ f : strategy1, m : {}, name : 'player 1', nr : 1 }, { f : strategy2, m : {}, name : 'player 2', nr : 2 }];
     options = options || {}
     options.n = options.n || 1000;
+    options.maxNrOfRounds = options.maxNrOfRounds || 10000; //NOTE: If both players strategy even does the bare minimum of not guessing already known wrong answers this caps out at n.
     options.getCorrectNr = options.getCorrectNr || function() {
         var min = 1;
         var max = options.n;
@@ -27,60 +19,49 @@ nrGsr = (function ()  {
     var logEnabled = options.logEnabled || false;  
     var correctNr = options.getCorrectNr();  
     var game = {
-      
+      winner : 0
     }
     
     if(logEnabled) {
       game.log = [];
     }
     
-    var isIn = function (nr, arrayOfNrs) {
-      for(var i=0; i< arrayOfNrs.length; i++) {
-        if(arrayOfNrs[i] === nr) {
-          return true;
-        }      
-      }
-      return false;
-    }
-    
-    var guesses = [];
-    var round = 0;
-    while(round < options.n) {
+    game.nrOfRounds = 1;
+    var lastGuess = null;
+    var lastResult = 0;
+    while(game.nrOfRounds <= options.maxNrOfRounds) {
       var guess;
       var current;
       var other;
+      
+      current = strats[1-(game.nrOfRounds%2)];
+      other = strats[game.nrOfRounds%2];
   
-      current = strats[round%2];
-      other = strats[1-(round%2)]; 
-  
-      guess = current.f(guesses.slice());
-  
-      if(isIn(guess, guesses) || guess < 1 || guess > options.n) {
+      guess = current.f(lastGuess, lastResult, current.m);
+      
+      if(guess === correctNr) {
         if(logEnabled) {
-          game.log.push(current.name + ': ' + guess + ' <-- INVALID');
-          game.log.push('--' + other.name + ' wins --');
-        }
-        game.nrOfRounds = round + 1;
-        game.winnerName = other.name;
-        game.invalidStrategyName = current.name;
-        return game;
-      } else if(guess === correctNr) {
-        if(logEnabled) {
-          game.log.push(current.name + ': ' + guess);
+          game.log.push(current.name +   ': ' + guess);
           game.log.push('--' + current.name + ' wins--');
         }
-        game.nrOfRounds = round + 1;
-        game.winnerName = current.name;
+        game.winner = current.nr;
         return game;
       } else {
-        guesses.push(guess);
+        lastGuess = guess;
+        if(guess < correctNr) {
+          lastResult = 1;
+        } else {
+          lastResult = -1;
+        }
         if(logEnabled) {
           game.log.push(current.name + ': ' + guess);
         }
       }
-      round = round + 1;
-    }  
-    throw 'game engine broke. no winner after all rounds.'
+      game.nrOfRounds = game.nrOfRounds + 1;
+    }
+    if(logEnabled) {
+      game.log.push('-- no winner. max nr of rounds hit --');
+    }
   }
   
   var findBestStrategy = function(strategy1, strategy2, options) {
